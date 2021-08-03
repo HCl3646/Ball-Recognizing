770037a88d9b8e644c240eb545af847793c0b977
HCl3646 <79964155+HCl3646@users.noreply.github.com>
2021-08-03 17:18:18 +0900

    Update RGB_Labeling.py

--- RGB_Labeling.py
+++ RGB_Labeling.py
@@ -12,8 +12,9 @@ if not video.isOpened():
     sys.exit()
 
 delay = df.getDelay(video)
-idx, count, score, score_0 = np.zeros(4)
+idx, score, score_0 = np.zeros(3)
 balls = list()
+
 while True:
     ret, img = video.read()
 
@@ -35,20 +36,19 @@ while True:
             cv2.line(src, (int(mx), int(my)), (int(mx), int(my)), (0, 0, 255), 3)
             point = (mx, my, idx)
             pts.append(point)
-            cv2.putText(src, str(pts.index(point)), (int(mx) + 1, int(my) - 1), 0, 1, (0, 255, 0), 1, cv2.LINE_AA,
-                        False)
+
                         
     for p in pts:
         cnt = 0
         length_0 = 9999999999
         temp = list()
-        boolean = True
+        moving = True
         for j in balls:
             length = df.distance(j[ - 1][0], j[- 1][1], p[0], p[1])
             if length > 200:
                 cnt += 1
-            elif length < 10:
-                boolean = False
+            elif length < 8:
+                moving = False
                 break
             else:
                 if length <= length_0:
@@ -58,25 +58,27 @@ while True:
                     continue
         if cnt == len(balls):
             balls.append([p])
-            count += 1
-        elif boolean is True and cnt != len(balls):
+        elif moving is True and cnt != len(balls):
             temp.append(p)
         else:
             continue
 
-    # 오랜시간 가만히 있던 공이 움직이는거 체크하는 것도 나쁘지 않을수도
     for i in balls:
         if len(i) >= 2 and idx == i[-1][2]:
             if len(i) >= 3 and df.distance(i[-1][0], i[-1][1], i[-2][0], i[-2][1]) >= 12:
                 g0 = df.gradient(i[- 2][0], i[- 2][1], i[- 3][0], i[- 3][1])
                 g1 = df.gradient(i[- 1][0], i[- 1][1], i[- 2][0], i[- 2][1])
-                if abs(math.atan(g0)-math.atan(g1)) >= math.pi/12 :
+                if abs(math.atan(g0)-math.atan(g1)) >= math.pi/12:
                     score += 1
                     break
                     
             elif i[-1][2] - i[-2][2] > delay * 7:
                 score += 1
                 break
+
+    for i in balls:
+        for j in range(len(i)-1):
+            cv2.line(src, (int(i[j][0]),int(i[j][1])),(int(i[j+1][0]),int(i[j+1][1])),(0,255,255),1)
     
     cv2.imshow('dst', src)
     idx += 1
